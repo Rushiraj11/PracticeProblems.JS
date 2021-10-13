2a90ce4c34e29ad361c56e72eab19aca297100c4
Rushiraj11 <grushiraj95@gmail.com>
2021-10-13 17:44:35 +0530

    Added: Power of 2 and coin flip program

--- Sequence problems.js
+++ Sequence problems.js
@@ -1,9 +1,11 @@
 console.log(Math.floor(Math.random() * 10)); // to get single digit 
 const Dice1= Math.floor(Math.random() * 6) + 1 ; // random dice number from 1 to 6
 const Dice2= Math.floor(Math.random() * 6) + 1 ;
-const A =Dice1+Dice2 ;
+const A =Dice1+Dice2 ;// addition of 2 random dice numbers
 console.log(A);
-var arr=[];
+
+
+var arr=[]; // generate 5 random numbers
 while(arr.length < 5){
     var r = Math.floor(Math.random() * 100) + 1;
     if(arr.indexOf(r) == -1) arr.push(r);
